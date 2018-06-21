1833affa170830c7e843abf82494bf5b40eb2f4d
WorK0Kurs <firs9821@gmail.com>
2018-06-22 01:03:57 +0700

    Fix menu to set the field

--- menu.c
+++ menu.c
@@ -52,6 +52,7 @@ void MenuStandart () {
 }
 
 void MenuInput () {
+    char garbage;
     int sizex=1, sizey=1;
     system ("clear");
     printf ("\n\n%56s", "Игра 'Жизнь'\n\n\n");
@@ -68,6 +69,7 @@ void MenuInput () {
         if (sizex==0) break;
         if (sizex<5 || sizex>25) printf ("%30s %s", " ", "Неверный размер! Введите заново: ");
     }
+    scanf("%c",&garbage);
     if (sizey!=0 && sizex!=0) Game (sizex, sizey);
     else MenuGame ();
 }
