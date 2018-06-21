45b5f526b118035ea8ad43fe00748374c8ffebea
WorK0Kurs <firs9821@gmail.com>
2018-06-22 00:44:38 +0700

    View the first generation

--- function.h
+++ function.h
@@ -4,6 +4,9 @@
 void Menu ();
 void MenuGame ();
 void MenuStandart ();
+void MenuInput ();
 void Game (int sizex, int sizey);
+void FillNull (int a[], int sizex, int sizey);
+void FillRand (int a[], int sizex, int sizey);
 
 #endif
